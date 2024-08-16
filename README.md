164a8288ef7b2674ae2960a94045c23670131c86
sudo-dqr <3577669403@qq.com>
2024-08-16 16:01:27 +0800

    init: 🎉

---
+++ README.md
@@ -0,0 +1,4 @@
+# <div align="center">Obito</div>
+
+​	Obito is a rust demo crate by sudo-dqr published on [crates.io](https://crates.io). Thanks to GuoQing Li, a friend I make during my undergraduate time in  SCSE, BUAA, who makse this demo crate more fun. This crate is mainly based on GuoQing Li's daily life.*You can use the operation in this crate to know about GuoQing Li loves to do with sudo-dqr.*:smiley:
+
---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//! Obito : GuoQing Li
+//! 
+//! The crate is a rust demo crate by sudo-dqr published on [crates.io](https://crates.io). 
+//! Thanks to GuoQing Li, a friend I make during my undergraduate time in  SCSE, BUAA, who makse this demo crate more fun. 
+//! This crate is mainly based on GuoQing Li's daily life.
+//! You can use the operations in this crate to know about what GuoQing Li loves to do with sudo-dqr.
+//! 
+//! 
